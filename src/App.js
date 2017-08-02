670b7ffb28e30b9e7693c6d3d827fd05491e18ce
mtraina <matteo.traina.mail@gmail.com>
2017-08-02 22:20:55 +0200

    nested jsx, returned enclosed into parentheses

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import React from 'react';
 // stateful
 class App extends React.Component {
   render(){
-    return <h1>Hello Series</h1>
+    return (
+      <div>
+        <h1>Hello Series</h1>
+        <b>Bold</b>
+      </div>
+      )
   }
 }
 
