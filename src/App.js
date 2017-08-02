d8cb64d86754737c9e5bcb32648ea195a64a4e9c
mtraina <matteo.traina.mail@gmail.com>
2017-08-02 22:17:42 +0200

    stateful and stateless class example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const App = () => <h1>Hi</h1>
+// stateful
+class App extends React.Component {
+  render(){
+    return <h1>Hello Series</h1>
+  }
+}
+
+// stateless
+//const App = () => <h1>Hi</h1>
 
 export default App
