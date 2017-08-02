a8e33cda22019311cc4d057cb6be8f9674b16f06
mtraina <matteo.traina.mail@gmail.com>
2017-08-02 22:24:23 +0200

    props

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ class App extends React.Component {
   render(){
     return (
       <div>
-        <h1>Hello Series</h1>
-        <b>Bold</b>
+        <h1>{this.props.txt}</h1>
       </div>
       )
   }
