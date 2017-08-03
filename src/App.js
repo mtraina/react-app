61cdfa0e269edcedaaa9c726381be45c46b3b30f
mtraina <matteo.traina.mail@gmail.com>
2017-08-03 21:14:57 +0200

    state I

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 // stateful
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      txt: 'this is the state txt'
+    }
+  }
+
   render(){
     let txt = this.props.txt
-    return <h1>{txt}</h1>
+    return <h1>{this.state.txt}</h1>
   }
 }
 
