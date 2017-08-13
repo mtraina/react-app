e097baefe40510efb068e591af35cf95900065dd
mtraina <matteo.traina.mail@gmail.com>
2017-08-13 12:55:21 +0200

    ref to access components I

--- src/App.js
+++ src/App.js
@@ -3,31 +3,21 @@ import React from 'react';
 class App extends React.Component {
   constructor(){
     super();
-    this.state = {currentEvent: '---'};
+    this.state = {a: ''};
     this.update = this.update.bind(this);
   }
 
   update(e){
-    this.setState({currentEvent: e.type});
+    this.setState({a: e.target.value});
   }
 
   render(){
     return (
       <div>
-      <textarea
-        onKeyPress={this.update}
-        onCopy={this.update}
-        onCut={this.update}
-        onPaste={this.update}
-        onFocus={this.update}
-        onBlur={this.update}
-        onDoubleClick={this.update}
-        onTouchStart={this.update}
-        onTouchMove={this.update}
-        onTouchEnd={this.update}
-        cols="30"
-        rows="10"
-      />
+      <input
+        type="text"
+        onChange={this.update}
+      /> {this.state.a}
       <h1>{this.state.currentEvent}</h1>
       </div>
     )
