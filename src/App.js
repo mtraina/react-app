92f641e4e7a0f8e1d1700235648b4be1ce08d048
mtraina <matteo.traina.mail@gmail.com>
2017-11-02 08:40:29 +0100

    compose react component behaviour with higher order components III

--- src/App.js
+++ src/App.js
@@ -2,10 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const HOC = (InnerComponent) => class extends React.Component {
+  constructor(){
+    super();
+    this.state = {count:0}
+  }
+
+  update(){
+    this.setState({count: this.state.count + 1})
+  }
+
+  componentWillMount(){
+    console.log('will mmount')
+  }
+
   render(){
     return (
       <InnerComponent
         {...this.props}
+        {...this.state}
+        update = {this.update.bind(this)}
       />
     )
   }
@@ -23,12 +38,20 @@ class App extends React.Component {
   }
 }
 
-const Button = HOC(props => <button>{props.children}</button>)
+const Button = HOC(props => 
+  <button onClick={props.update}>{props.children} - {props.count}</button>
+)
 
 class Label extends React.Component {
+  componentWillMount(){
+    console.log('Label will mmount')
+  }
+
   render(){
     return (
-      <label>{this.props.children}</label>
+      <label onMouseMove={this.props.update}>
+        {this.props.children} - {this.props.count}
+      </label>
     )
   }
 }
